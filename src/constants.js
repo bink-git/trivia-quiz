e40c25a6e715d67ef5e42aed5ac6f6a0f4b147fb
BINK <bink@email.ua>
2024-01-23 17:03:04 +0200

    fix

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const API_URL =
-  'https://opentdb.com/api.php?amount=10&category=9&difficulty=eay&type=multiple';
+  'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple';
