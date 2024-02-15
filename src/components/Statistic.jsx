c0ae7a8221d5d4449c3ab8f0d0cb609328af0513
BINK <bink@email.ua>
2024-02-15 15:28:04 +0200

    fix

--- src/components/Statistic.jsx
+++ src/components/Statistic.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Statistic = ({ statistic }) => {
   return (
     <div className="statistic">
--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyD-j9jmDAxXqtkoLf1iFqVyhLd5qP11WfI',
@@ -11,6 +12,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-export { auth };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
