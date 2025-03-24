1a40a2180e5b3d8163c0d5a555c5be72c9b79fd2
bink-git <bink@email.ua>
2025-03-24 15:34:48 +0100

    fix

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import he from "he";
 import { useSharedState } from "../context/sharedContext";
 import { Button } from "./ui/button";
